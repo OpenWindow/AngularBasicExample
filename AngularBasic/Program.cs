3ad9b6091dd31d850816326bdaaf5bc78299a4f4
Neel <neelavardhanreddym@gmail.com>
2017-10-25 22:57:16 -0400

    added comments example

--- AngularBasic/Program.cs
+++ AngularBasic/Program.cs
@@ -14,6 +14,13 @@ namespace AngularBasic
     {
         public static void Main(string[] args)
         {
+            // this is a comment single line
+            // sdfsdf
+
+            /* 
+              this is a multiline
+              comment
+             */
             BuildWebHost(args).Run();
         }
 
